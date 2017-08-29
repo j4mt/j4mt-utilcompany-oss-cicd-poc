9903fb8cc5fd794cfe38bdada3f3018fe282ce97
j4mt <john.orourke.1985@gmail.com>
2017-08-29 22:47:40 +0100

    adding error page view controller

--- oss/src/main/java/com/j4mt/oss/config/MvcConfig.java
+++ oss/src/main/java/com/j4mt/oss/config/MvcConfig.java
@@ -11,6 +11,7 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/error").setViewName("error");
     }
 
 }
